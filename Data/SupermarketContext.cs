e2ced077ee7ad672b0a49b5646df2980a71c7395
Samuel Giraldo <samgirari@gmail.com>
2025-05-06 22:09:52 -0500

    Se añade la carpeta Data y la clase SupermarketContext y se añade lo infromado en el manual.

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Data
+{
+    class SupermarketContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+        }
+    }
+}
