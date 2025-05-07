835e7913246f856fd30244440b4292365d8987a6
Samuel Giraldo <samgirari@gmail.com>
2025-05-06 23:27:24 -0500

    se termina la parte de program.cs como dice el manual.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using SupermarketEF.Data;
+using SupermarketEF.Models;
 
 namespace SupermarketEF
 {
@@ -8,6 +9,34 @@ namespace SupermarketEF
         {
             using SupermarketContext context = new SupermarketContext();
             //Se crea el objeto oilCategory de tipo Category
+            Category oilCategory = new Category
+            {
+                Name = "Oil",
+                Description = "Oil Category"
+            };
+
+            // Se agrega el objeto creado al contexto de la BD
+            // Usando la propiedad Categories del contexto
+            context.Categories.Add(oilCategory);
+
+            // Se crea el objeto grainsCategory de tipo Category
+            Category grainsCategory = new Category()
+            {
+                Name = "Grains",
+                Description = "Grains Category"
+            };
+
+            // Se agrega el objeto creado al contexto de la BD
+            context.Add(grainsCategory);
+
+            // Se graban los cambios realizados al contexto
+            context.SaveChanges();
+            // Recupera todas las categorías y las ordena por el nombre
+            var categories = context.Categories.OrderBy(p => p.Name);
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"{category.Name} | {category.Description}");
+            }
         }
     }
 }
