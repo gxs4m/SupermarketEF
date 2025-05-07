7c54473f0ebe7cb20d4509771c7ce9c5a8e2bc3b
Samuel Giraldo <samgirari@gmail.com>
2025-05-06 21:48:50 -0500

    Agregar archivos de proyecto.

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Models
+{
+    class Product
+    {
+        // [Key] -> Anotación si la propiedad no se llama Id, ejemplo ProductId
+        public int Id { get; set; } // Será la llave primaria 
+    
+    }
+}
